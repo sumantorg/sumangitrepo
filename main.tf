0cf7990538539af941f09e3a85ebe3b090dd4bf5
sumantripathi <suman.tripathi@insight.com>
2025-07-08 12:36:10 +0530

    new rg2 added

--- main.tf
+++ main.tf
@@ -6,5 +6,8 @@ resource "azurerm_resource_group" "sum-rgs" {
   resource "azurerm_resource_group" "sum-rgs1" {
   name     = sumanrgs1
   location = central india
-
+  }
+    resource "azurerm_resource_group" "sum-rgs2" {
+  name     = sumanrgs2
+  location = central india
   }
